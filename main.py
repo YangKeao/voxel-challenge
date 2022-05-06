2e58c109f920430e39f8bf2adb2d3c421008b540
YangKeao <yangkeao@chunibyo.icu>
2022-05-06 16:01:28 +0000

    accelerate the computing through ti.static
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def draw_sin_curve(translation,r_axis,r_angle,color,period,phase,length,scale,ra
 
 @ti.kernel
 def initialize_voxels():
-    for line_y in range(3):
+    for line_y in ti.static(range(3)):
         translation=vec3(-icon_size/2,line_y*icon_size/2-icon_size/2+shs/2-(line_y-1)*margin,0)+icon_center
         skip=vec2(0.0, 0.0)
         if line_y == 2:
@@ -65,25 +65,24 @@ def initialize_voxels():
             skip=vec2(icon_size/2+24, icon_size/2+34)
         draw_sin_curve(translation, vec3(1, 0, 0), 0.0, line_color,
                        icon_size * 2, pi, icon_size, shs, line_radius,skip)
-    for line_x in range(3):
+    for line_x in ti.static(range(3)):
         translation=vec3(line_x*icon_size/2-icon_size/2+svs/2-(line_x-1)*margin,-icon_size/2,0)+icon_center
         draw_sin_curve(translation, vec3(0, 0, 1), pi / 2, line_color,
                        icon_size, pi, icon_size, svs, line_radius)
-    for x in range(3):
-        for y in range(3):
-            pos_x = -icon_size/2+margin+x*(icon_size-2*margin)/2
-            pos_x -= svs*ti.sin((y*(icon_size-2*margin)/2+margin)/icon_size*2*pi+pi)-svs/2
-            pos_y = -icon_size / 2 + margin + y * (icon_size - 2*margin)/2
-            pos_y += shs*ti.sin((x*(icon_size-2*margin)/2+margin)/icon_size*pi+pi)+shs/2
-            if x == 1 and y == 1:
-                draw_disc(vec3(pos_x, pos_y, 0),big_sphere_radius+disc_extra_radius,line_color,disc_z_range)
-                draw_sphere(vec3(pos_x, pos_y, 0),big_sphere_radius, whilte_sphere_color,empty=dark_mode)
-            elif x == 1 or y == 1:
-                draw_disc(vec3(pos_x, pos_y, 0),sphere_radius+disc_extra_radius,line_color,disc_z_range)
-                draw_sphere(vec3(pos_x, pos_y, 0),sphere_radius,blue_sphere_color)
-            else:
-                draw_disc(vec3(pos_x, pos_y, 0),sphere_radius+disc_extra_radius,line_color,disc_z_range)
-                draw_sphere(vec3(pos_x, pos_y, 0),sphere_radius,pink_sphere_color)
+    for x,y in ti.static(ti.ndrange((0,3), (0,3))):
+        pos_x = -icon_size/2+margin+x*(icon_size-2*margin)/2
+        pos_x -= svs*ti.sin((y*(icon_size-2*margin)/2+margin)/icon_size*2*pi+pi)-svs/2
+        pos_y = -icon_size / 2 + margin + y * (icon_size - 2*margin)/2
+        pos_y += shs*ti.sin((x*(icon_size-2*margin)/2+margin)/icon_size*pi+pi)+shs/2
+        if x == 1 and y == 1:
+            draw_disc(vec3(pos_x, pos_y, 0),big_sphere_radius+disc_extra_radius,line_color,disc_z_range)
+            draw_sphere(vec3(pos_x, pos_y, 0),big_sphere_radius, whilte_sphere_color,empty=dark_mode)
+        elif x == 1 or y == 1:
+            draw_disc(vec3(pos_x, pos_y, 0),sphere_radius+disc_extra_radius,line_color,disc_z_range)
+            draw_sphere(vec3(pos_x, pos_y, 0),sphere_radius,blue_sphere_color)
+        else:
+            draw_disc(vec3(pos_x, pos_y, 0),sphere_radius+disc_extra_radius,line_color,disc_z_range)
+            draw_sphere(vec3(pos_x, pos_y, 0),sphere_radius,pink_sphere_color)
 
 initialize_voxels()
 scene.finish()
