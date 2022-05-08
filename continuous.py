a57b037ce3d7d63cc929981084ae79bb3fab7259
YangKeao <yangkeao@chunibyo.icu>
2022-05-08 03:26:54 +0000

    add continous version of plykin attractor
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- continuous.py
+++ continuous.py
@@ -1,66 +1,97 @@
+from functools import total_ordering
 import taichi as ti
+from taichi.lang.kernel_impl import kernel
 from taichi.math import *
 
-ti.init(arch=ti.gpu)
+ti.init(arch=ti.cpu)
 
-radius = 520
+radius = 512
 sphere_bias = ti.sqrt(3)
 
+point_num = 1024 * 1024 * 4
+point = ti.Vector.field(3, dtype=ti.f32, shape=point_num)
+step_length = 0.05
+
 pixels = ti.field(dtype=float, shape=(radius*2, radius*2))
-attract_steps = 720
+
+total_period = 120
+step_period = total_period / 4
 
 @ti.func
-def f(p, sign, sigma):
-    x = sign * p.z
-    y = ti.sqrt(p.xy.dot(p.xy))*(
-        p.y * ti.exp(sigma / 2 * (p.xy.dot(p.xy))) *
-        ti.cos(pi / 2 * (p.z * ti.sqrt(2) + 1))
-        + sign * p.x * ti.exp(-sigma/2 * (p.xy.dot(p.xy))) *
-        ti.sin(pi / 2 * (p.z * ti.sqrt(2) + 1))
-    ) / ti.sqrt(
-        p.x * p.x * ti.exp(-sigma * p.xy.dot(p.xy)) + p.y *
-        p.y * ti.exp(sigma * p.xy.dot(p.xy))
-    )
-    z = ti.sqrt(p.xy.dot(p.xy))*(
-        p.y * ti.exp(sigma / 2 * (p.xy.dot(p.xy))) *
-        ti.sin(pi / 2 * (p.z * ti.sqrt(2) + 1))
-        - sign * p.x * ti.exp(-sigma/2 * (p.xy.dot(p.xy))) *
-        ti.cos(pi / 2 * (p.z * ti.sqrt(2) + 1))
-    ) / ti.sqrt(
-        p.x * p.x * ti.exp(-sigma * p.xy.dot(p.xy)) + p.y *
-        p.y * ti.exp(sigma * p.xy.dot(p.xy))
-    )
-    return vec3(x, y, z)
+def step(p, sigma, time):
+    if time >= 0 and time < step_period:
+        diff = time
 
+        x_ = -(sigma * p.x * p.y * p.y) * step_length
+        y_ = (sigma * p.x * p.x * p.y) * step_length
+        p.x += x_
+        p.y += y_
+        p = p.normalized()
 
-@ti.func
-def step(p, sigma):
-    negative = f(p, -1.0, sigma)
-    positive = f(negative, 1.0, sigma)
-    return positive
+    if time >= step_period and time < step_period * 2:
+        diff = time - step_period
+
+        p = rotate3d(p, vec3(0, 0, 1), (p.z - (-ti.sqrt(2)/2)) / ti.sqrt(2) * pi * diff / step_period)
+
+    if time >= step_period * 2 and time < step_period * 3:
+        diff = time - step_period * 2
+
+        y_ = sigma * p.y * p.z * p.z * step_length
+        z_ = -sigma * p.y * p.y * p.z * step_length
+        p.y += y_
+        p.z += z_
+        p = p.normalized()
+
+    if time >= step_period * 3 and time < step_period * 4:
+        diff = time - step_period * 3
+        p = rotate3d(p, vec3(1, 0, 0), (p.x - (-ti.sqrt(2)/2)) / ti.sqrt(2) * pi * diff / step_period)
+    
+    return p
 
 @ti.kernel
-def paint():
+def random_points():
+    for i in range(point_num):
+        point[i].x = (ti.random() - 0.5) * 2
+        point[i].y = (ti.random() - 0.5) * 2
+        point[i].z = (ti.random() - 0.5) * 2
+        point[i] = point[i].normalized()
+
+@ti.func
+def render_point(p):
+    p_3d = p * radius
+    p_3d = rotate3d(p_3d, vec3(0, 0, 1), -pi / 4)
+    p_3d = rotate3d(p_3d, vec3(0, 1, 0), pi / 4)
+
+    p_3d.yz += radius
+    if p_3d.x < 0:
+        pixels[p_3d.y, p_3d.z] = 0.6
+    else:
+        pixels[p_3d.y, p_3d.z] = 1
+
+@ti.kernel
+def paint(time: ti.i32):
     for i, j in ti.ndrange((-radius, radius), (-radius, radius)):
-        pixels[i+radius, j+radius] = 0
-        if i * i + j * j < radius * radius:
-            k = ti.sqrt(radius * radius - i * i - j * j)
-            p_3d = vec3(i, j, k)
-            p = vec3(i, j, k)/radius
-            for _ in range(attract_steps):
-                p = step(p, 1.0)
-            p_3d = p.normalized() * radius
-            p_3d.xy += radius
-            if p_3d.z < 0:
-                pixels[p_3d.x, p_3d.y] = 0.2
-            else:
-                pixels[p_3d.x, p_3d.y] = 1
+        pixels[i + radius, j + radius] = 0
+
+    for i in range(point_num):
+        p = step(point[i], 1.0, time % total_period)
+        if time % step_period == step_period - 1 or time % (2 * step_period) < step_period:
+            point[i] = p
+        render_point(p)
+
+random_points()
 
 gui = ti.GUI("Plykin Attractors", res=(radius*2, radius*2))
 
 i = 0
+for _ in range(2000):
+    paint(i)
+    i = i + 1
+    if i%10 == 0:
+        print("precalculate: ", i)
+
 while gui.running:
-    paint()
+    paint(i)
     gui.set_image(pixels)
     gui.show()
     i = i + 1
